3e8262e62cc721e11744f4e94f00d54167d44f6e
schl0014 <schl0014@hz.nl>
2022-02-09 09:03:10 +0100

    laravel plus oude site

---
+++ resources/views/navigation.blade.php
@@ -0,0 +1,9 @@
+
+<!--navigatiebar  -->
+<nav class="navbar">
+    <a href="/">Home</a>
+    <a href="profile">Profile</a>
+    <a href="dashboard">Dashboard</a>
+    <a href="faq">FAQ</a>
+    <a href="blog">Blog</a>
+</nav>
