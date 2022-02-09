5ae83b2b16db519ed65dc7b63ddd9d7dea5a8132
schl0014 <schl0014@hz.nl>
2022-02-09 09:20:39 +0100

    laravel plus oude site

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -6,6 +6,9 @@ use Illuminate\Http\Request;
 
 class BlogController extends Controller
 {
+    /**
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View go to blog view
+     */
     public function show(){
         return view('blog');
     }
